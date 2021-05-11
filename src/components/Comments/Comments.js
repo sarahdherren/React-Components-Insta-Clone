97aed78fd6c502b0897be1d9bb3f5c8f60a34bdb
Sarah D Herren <herren.sarah@icloud.com>
2021-05-11 13:07:42 -0500

    mvp

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,9 +5,14 @@ import './Comments.css';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+  console.log(comments)
   return (
     <div>
+      {comments.map(comment => {
+        return (
+          <Comment comment={comment} />
+        )
+      })}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
